f5f22e9cd1da6eee0e3599ca5c71541ee10b8244
xschen <xs0040@gmail.com>
2017-09-02 11:32:43 +0800

    progress update

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Redux, React Router for Magento 2 Demo site</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;

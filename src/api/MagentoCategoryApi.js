2ec1af0f4b9b2cc34aeb07485c619a89bd522c7a
xschen <xs0040@gmail.com>
2017-09-06 10:58:03 +0800

    successfully load category product

--- src/api/MagentoCategoryApi.js
+++ src/api/MagentoCategoryApi.js
@@ -5,6 +5,9 @@ class MagentoCategoryApi {
     static loadRootCategory(siteUrl, token){
         return MagentoHttpClient.getSecure(siteUrl + MagentoCategoryApi.Path4Categories, token);
     }
+    static loadProducts(siteUrl, categoryId, token){
+        return MagentoHttpClient.getSecure(siteUrl + MagentoCategoryApi.Path4Categories + '/' + categoryId + '/products', token);
+    }
 }
 
 MagentoCategoryApi.Path4Categories = "/rest/V1/categories";
